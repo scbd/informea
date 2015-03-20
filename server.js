e74adba1e8f8a97570552c2a0262d779048a287d
Philippe Leblond <leblond.philippe@gmail.com>
2015-03-20 10:33:15 -0400

    implement checkPermission

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ console.log('Starting OData server.');
 
 $data.createODataServer({
     type: require('./context.js'),
+    checkPermission: function(access, user, entitySets, callback) {
+        
+        if(access & $data.Access.Read) callback.success();
+        else                           callback.error('auth fail');
+    }
 }, '/informea.svc', 5000, 'localhost');
 
 console.log('OData server listening on http://localhost:5000/informea.svc');
