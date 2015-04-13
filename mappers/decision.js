63fbb9225c21431fed48ce580839323d0328e655
Philippe Leblond <leblond.philippe@gmail.com>
2015-04-13 16:23:38 -0400

    final changes to 'Decision' mapper

--- mappers/decision.js
+++ mappers/decision.js
@@ -9,23 +9,23 @@ class DecisionMapper {
     //============================================================
     async query () {
         
-    	var res = await superagent.get('https://chm.cbd.int/api/v2013/index/select?fl=id,title_t&q=realm_ss:chm+AND+schema_s:decision&rows=25&sort=createdDate_dt+desc,+title_t+asc&start=0&wt=json').use(superagentQ).end();
+    	var res = await superagent.get('https://chm.cbd.int/api/v2013/index/select?fl=id,title_t,code_s,createdDate_dt,updatedDate_dt&q=realm_ss:chm+AND+schema_s:decision&rows=25&sort=createdDate_dt+desc,+title_t+asc&start=0&wt=json').use(superagentQ).end();
 
         return res.body.response.docs.map(document => new Decision({
             
             protocolVersion:        1,       
             id:                     document.id,
-        //  link:                   required
+            link:                   'http://www.cbd.int/decision/cop/default.shtml?id=' + parseInt(document.id.substring(16), 16),
             title:                  [ { language: 'en', value: document.title_t } ],
         //  longTitle:              
         //  summary:                
-        //  type:                   required
-        //  status:                 required
-        //  number:                 required
-            treaty:                 'cbd'
-        //  published:              required
-        //  updated:                
-        //  meetingId:              
+            type:                   'decision',
+            status:                 'active',
+            number:                 document.code_s,
+            treaty:                 'cbd',
+            published:              document.createdDate_dt,
+            updated:                document.updatedDate_dt
+        //  meetingId:
         //  meetingTitle:           
         //  meetingUrl:             
         //  content:                
