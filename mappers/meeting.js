bb0a26c90749a7c32c3d3e9a6fc6c890c686ff08
Philippe Leblond <leblond.philippe@gmail.com>
2015-04-15 16:49:11 -0400

    minor fixes

--- mappers/meeting.js
+++ mappers/meeting.js
@@ -9,7 +9,7 @@ class MeetingMapper {
     //============================================================
     async query () {
         
-    	var url = "https://chm.cbd.int/api/v2013/index/select?fl=id,title_t,updatedDate_dt,url_ss&q=NOT+version_s:*+AND+realm_ss:chm+AND+schema_s:meeting&rows=99999&start=0&wt=json"
+    	var url = 'https://chm.cbd.int/api/v2013/index/select?fl=id,title_t,updatedDate_dt,url_ss&q=NOT+version_s:*+AND+realm_ss:chm+AND+schema_s:meeting&rows=100&start=0&wt=json';
 
     	var res = await superagent.get(url).use(superagentQ).end();
 
@@ -18,7 +18,7 @@ class MeetingMapper {
             protocolVersion:        1,
             id:                     document.id,
             treaty:                 'cbd',
-        //  url:                    { type: String,                                 required: false },   // Link where the meeting details are published.
+            url:                    document.url_ss.length ? document.url_ss[0] : null,
             title:                  [ { language: 'en', value: document.title_t } ],
         //  description:            { type: Array, elementType: LocalizableString,  required: false },   // Short description of the meeting. This is a list of Localizable String complex type objects.
             start:                  document.startDate_dt,
@@ -32,7 +32,7 @@ class MeetingMapper {
         //  imageCopyright:         { type: String,                                 required: false },   // Copyright statement of the above image
         //  location:               { type: String,                                 required: false },   // Location of the event (ex. Nottingham Castle, grand ball room)
             city:                   document.eventCity_t,
-            country:                document.eventCountry_s,
+            country:                (document.eventCountry_s||'').toUpperCase(),
         //  latitude:               { type: Number,                                 required: false },   // Latitude of the event place in decimal degrees (ex. 23.456323). This value may be used to represent the event on a map using existing geographical applications such as Google Maps
         //  longitude:              { type: Number,                                 required: false },   // Longitude of the event place in decimal degrees (ex. 23.456323). This value may be used to represent the event on a map using existing geographical applications such as Google Maps
             updated:                document.updatedDate_dt
