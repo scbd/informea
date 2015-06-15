f8cfc60a9ea350849f69a742e1381410facba45b
Philippe Leblond <leblond.philippe@gmail.com>
2015-06-15 10:39:47 -0400

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t registry.infra.cbd.int:5000/informea git@github.com:scbd/informea.git
-docker push registry.infra.cbd.int:5000/informea
+docker build -t localhost:5000/informea git@github.com:scbd/informea.git
+docker push localhost:5000/informea
