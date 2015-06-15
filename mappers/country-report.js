fb5722f678fac832a0431b0f119343a55b1b9066
Philippe Leblond <leblond.philippe@gmail.com>
2015-06-15 14:35:23 -0400

    Add files to Country Reports

--- mappers/country-report.js
+++ mappers/country-report.js
@@ -9,7 +9,7 @@ class CountryReportMapper {
     //============================================================
     async query () {
         
-    	var res = await superagent.get('https://chm.cbd.int/api/v2013/index/select?fl=id,government_s,title_t,createdDate_dt,url_ss,updatedOn_dt&q=realm_ss:chm+AND+schema_s:nationalReport&rows=9999&sort=createdDate_dt+desc,+title_t+asc&start=0&wt=json').use(superagentQ).end();
+    	var res = await superagent.get("https://chm.cbd.int/api/v2013/index/select?fl=id,title_t,government_s,createdDate_dt,updatedDate_dt,url_ss,reportType_s,documentLinks_s&q=NOT+version_s:*+AND+realm_ss:chm+AND+schema_s:*+AND+(+schema_s:nationalReport+)+AND+(*:*+NOT+reportType_s:B0EBAE91-9581-4BB2-9C02-52FCF9D82721+)&rows=9999&start=0&wt=json").use(superagentQ).end();
 
         return res.body.response.docs.map(document => new CountryReport({
                     
@@ -19,14 +19,10 @@ class CountryReportMapper {
             country:            document.government_s.toUpperCase(),
             title:              [ { language: 'en', value: document.title_t } ],
             submission:         document.createdDate_dt,
-        //  url:                document....,
-            files:              (document.url_ss||[]).map(url => new File({
-            //  id:
-                url:            url
-            //  content:
-            //  mimeType:
-            //  language:
-            //  filename:
+            url:                document.url_ss[0],
+            files:              JSON.parse(document.documentLinks_s||'[]').map(link => new File({
+                filename: link.name,
+                url: link.url
             })),
             updated:            document.updatedOn_dt
         }));
