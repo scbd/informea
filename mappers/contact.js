27416cb3a3d5f97f570b2302df86622805c8caeb
Stephane Bilodeau <bilodeaustephane@gmail.com>
2016-06-03 17:21:52 -0400

    Fix Solution, FirstName, LastName & Address

--- mappers/contact.js
+++ mappers/contact.js
@@ -10,13 +10,13 @@ class ContactMapper {
     async query () {
 
     	//var url = 'https://chm.cbd.int/api/v2013/index/select?fl=id,ctgList_ss,government_s,title_t,function_t,organization_t,department_t,email_ss,telephone_s,fax_ss,updatedDate_dt&q=schema_s:focalPoint+AND+(ctgList_ss:CBD-FP1+OR+ctgList_ss:CBD-FP2+OR+ctgList_ss:CPB-FP1+OR+ctgList_ss:ABS-FP)&rows=9999&start=0&wt=json';
-        var url = 'https://chm.cbd.int/api/v2013/index/select?fl=id,ctgList_ss,government_s,title_t,function_t,organization_t,department_t,email_ss,telephone_s,fax_ss,updatedDate_dt&q=schema_s:focalPoint+AND+(ctgList_ss:CBD-FP1+OR+ctgList_ss:CBD-FP2+OR+ctgList_ss:SBSTTA-FP+OR+ctgList_ss:CHM-FP+OR+ctgList_ss:CHM-IAC+OR+ctgList_ss:GSPC-FP+OR+ctgList_ss:GTI-FP+OR+ctgList_ss:PA-FP+OR+ctgList_ss:RM-FP+OR+ctgList_ss:TKBD-FP+OR+ctgList_ss:CPB-FP1+OR+ctgList_ss:CPB-FP2+OR+ctgList_ss:BCH-FP+OR+ctgList_ss:ABS-FP+OR+ctgList_ss:ABS-FP2+OR+ctgList_ss:ABSCH-IAC)&rows=9999&start=0&wt=json';
+        var url = 'https://chm.cbd.int/api/v2013/index/select?fl=id,ctgList_ss,government_s,title_t,function_t,organization_t,department_t,email_ss,telephone_s,fax_ss,updatedDate_dt,salutation_s,firstName_s,lastName_s,address_t&q=schema_s:focalPoint+AND+(ctgList_ss:CBD-FP1+OR+ctgList_ss:CBD-FP2+OR+ctgList_ss:SBSTTA-FP+OR+ctgList_ss:CHM-FP+OR+ctgList_ss:CHM-IAC+OR+ctgList_ss:GSPC-FP+OR+ctgList_ss:GTI-FP+OR+ctgList_ss:PA-FP+OR+ctgList_ss:RM-FP+OR+ctgList_ss:TKBD-FP+OR+ctgList_ss:CPB-FP1+OR+ctgList_ss:CPB-FP2+OR+ctgList_ss:BCH-FP+OR+ctgList_ss:ABS-FP+OR+ctgList_ss:ABS-FP2+OR+ctgList_ss:ABSCH-IAC)&rows=9999&start=0&wt=json';
     	var res = await superagent.get(url).end();
 
 		return res.body.response.docs.map(document => {
 
             var treaties = [];
-            var primary  = 1;            
+            var primary  = 1;
             var roles = [];
             var cbdRoles=[];
             var cpbRoles=[];
@@ -149,7 +149,7 @@ class ContactMapper {
                 country:            document.government_s.toUpperCase(),
                 prefix:             document.salutation_s,
                 firstName:          document.firstName_s,
-                lastName:           document.title_t,
+                lastName:           document.lastName_s,
             //  type:               'N/A',
                 position:           document.function_t,
                 institution:        document.organization_t,
