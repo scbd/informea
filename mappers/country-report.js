c98dbfd49cd785b91149a4e3abd1d6cd74b1c299
Blaise Fonseca <blaise.fonseca@un.org>
2025-08-11 16:05:01 -0400

    Fix nbsap query to inlcude to chm nsbsap and ort

--- mappers/country-report.js
+++ mappers/country-report.js
@@ -9,7 +9,15 @@ class CountryReportMapper {
     //============================================================
     async query () {
 
-    	var res = await superagent.get("https://chm.cbd.int/api/v2013/index/select?fl=id,title_t,government_s,createdDate_dt,updatedDate_dt,url_ss,reportType_s,documentLinks_s&q=NOT+version_s:*+AND+realm_ss:chm+AND+schema_s:*+AND+(+schema_s:nationalReport nationalReport6+)+AND+(*:*+NOT+reportType_s:B0EBAE91-9581-4BB2-9C02-52FCF9D82721+)&rows=9999&start=0&wt=json");
+        const url = new URL("https://chm.cbd.int/api/v2013/index/select");
+        url.searchParams.set('fl', 'id,title_t,government_s,createdDate_dt,updatedDate_dt,url_ss,reportType_s,documentLinks_s');
+        url.searchParams.set('q', 'NOT version_s:* AND ((realm_ss:chm AND schema_s:(nbsap nationalReport6)) OR (realm_ss:ort AND schema_s:nbsap))');
+        url.searchParams.set('rows', '9999');
+        url.searchParams.set('start', '0');
+        url.searchParams.set('wt', 'json');     
+    	var res = await superagent.get(url.toString())
+            .set('Accept', 'application/json')
+            .set('User-Agent', 'INFORMEA-CONNECTOR-API/1.0');
 
         return res.body.response.docs.map(document => new CountryReport({
 
